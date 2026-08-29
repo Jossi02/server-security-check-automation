8bb4e84679ecafd91249aa0897b66f298bb3729d
Jossi02 <02love326@kyonggi.ac.kr>
2026-08-29 22:36:46 +0900

    Align guide history and validation notes

--- windows/W-50.Parser.ps1
+++ windows/W-50.Parser.ps1
@@ -1,4 +1,4 @@
-function ConvertFrom-NetAccountsMaximumAge {
+﻿function ConvertFrom-NetAccountsMaximumAge {
     param([Parameter(Mandatory)][string[]]$Lines)
 
     $line = $Lines | Where-Object { $_ -match '^\s*(Maximum password age|최대 암호 사용 기간)(\s*\([^)]*\))?\s*:' } | Select-Object -First 1
